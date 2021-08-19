7be5bf2a91dcc54c42127133d56648483d6f86f7
王艳林 <981247127@qq.com>
2021-08-19 13:55:43 +0800

    修改 Git 测试类

--- src/test/java/com/tedu/hadoop/test/git/GitTest.java
+++ src/test/java/com/tedu/hadoop/test/git/GitTest.java
@@ -2,11 +2,13 @@ package com.tedu.hadoop.test.git;
 
 import com.tedu.hadoop.mr.common.ConfigurationManager;
 import org.apache.hadoop.conf.Configuration;
+import org.junit.Test;
 
 public class GitTest {
-	public static void main(String[] args) {
+	@Test
+	public void testConfiguration() {
 		Configuration configuration = ConfigurationManager.get();
-		System.out.println(configuration.get("fs.defaultFS"));
+		System.out.println(configuration.get("fs.defaultFS", "file://"));
 		System.out.println("wrote by hot-fix");
 	}
 }
